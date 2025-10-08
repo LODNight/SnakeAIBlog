a95a00dff9eba612700e4c7ce716480e8aa27ddb
LODNight <khuunhattin123@gmail.com>
2025-10-08 11:56:25 +0700

    done with first django project

--- backend/blog_generator/views.py
+++ backend/blog_generator/views.py
@@ -123,23 +123,19 @@ def get_transcription(link):
     return transcript.text
 
 
-# genai.configure(api_key=settings.GEMINI_API_KEY)
+genai.configure(api_key=settings.GEMINI_API_KEY)
 def generate_blog_from_transcription(transcription):
     
-    # prompt = f"Based on the following transcript from a YouTube video, write a comprehensive blog article, write it based on the transcript, but dont make it look like a youtube video, make it look like a proper blog article:\n\n{transcription}\n\nArticle:"
+    prompt = f"Based on the following transcript from a YouTube video, write a comprehensive blog article, write it based on the transcript, but dont make it look like a youtube video, make it look like a proper blog article:\n\n{transcription}\n\nArticle:"
 
-    # try:
-    #     model = genai.GenerativeModel("gemini-2.5-flash")
-    #     response = model.generate_content(prompt)
-    # except Exception as e:
-    #     print(" gemini-2.5-flash lỗi:", e)
-    #     response = model.generate_content(prompt)
+    try:
+        model = genai.GenerativeModel("gemini-2.5-flash")
+        response = model.generate_content(contents=prompt)
+    except Exception as e:
+        print(" gemini-2.5-flash lỗi:", e)
+        response = model.generate_content(prompt)
 
-    # return response.text
-    print(transcription)
-    print("+====+")
-    return "Hello"
-
+    return response.text
 
 # blog-list
 def blog_list(request):
